a59479c2cabd6a06a7d744c3e6066f8e4d2828d9
Marisol Poma <marisol.poma15@hotmail.com>
2019-08-30 22:02:21 -0300

    Data binding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+  //adicionamos na aula 30/08/2019
+
+  altura  = 1.70
+  peso = 70
+  resultado = 'imc'
+  altural = 'Peso: 1.70'
+  pesot = 'Peso: 70'
+
+  imc():void { 
+    let imc = this.peso/(this.altura * this.altura)
+    alert('imc: '+ imc );  
+  }
 }
